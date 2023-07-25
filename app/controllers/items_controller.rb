b56d0e755f8816db002021e14d63855d66b1527e
Haine15tot <haine.15425@gmail.com>
2023-07-25 21:15:30 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :ensure_owner, only: [:edit, :update]
+  before_action :ensure_logged_in, only: [:edit, :update]
 
   def index
     @item = Item.order(created_at: :desc)
@@ -7,6 +9,19 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: "商品情報を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -27,6 +42,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :items_name, :item_text, :category_id, :condition_id, :load_id, :prefecture_id, :delivery_id, :price).merge(user_id: current_user.id)
   end
+  
+def ensure_owner
+  @item = Item.find(params[:id])
+  redirect_to root_path, alert: "他のユーザーの商品は編集できません。" unless @item.user == current_user
+end
+
+def ensure_logged_in
+  redirect_to login_path, alert: "ログインが必要です。" unless user_signed_in?
+end
 end
 
 
